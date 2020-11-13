3579a72bac79e1dde73ada7b24291026fe489201
Edouard Lavidalle <34841632+EdouardLvdl@users.noreply.github.com>
2020-11-13 13:57:30 +0100

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://condescending-bartik-a219df.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `http://beaverwriter-blog.netlify.app.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
