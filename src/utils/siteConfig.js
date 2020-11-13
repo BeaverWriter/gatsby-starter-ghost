d0950b4b195a4163b7f27c74e19e8053bda3f3ee
EdouardLvdl <edouardlavidalle@gmail.com>
2020-11-13 17:32:29 +0100

    Update siteUrl

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://beaverwriter-blog.netlify.app.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://blog.beaverwriter.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
